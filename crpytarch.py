69b5b3ed73acfa61727a45ad30df75059814b76e
GuiltedRose <alisonroseclark001@gmail.com>
2022-01-11 20:29:18 -0500

    forth commit

--- crpytarch.py
+++ crpytarch.py
@@ -1,7 +1,6 @@
 # This will the base file for our encription method
 import string
-import unicodedata
 import os
-
+import pypi
 # algorithms to know: RSA should be good enough if we decide to make it a web app or something.
 # Why RSA? Because it's used for SSH, HTTPS, Bitcoin, and PGP/GPG encryption.
