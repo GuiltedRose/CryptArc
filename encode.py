8c7abce771132c5d6611e9f1f0490dfa9da0fd13
GuiltedRose <alisonroseclark001@gmail.com>
2022-01-11 19:52:14 -0500

    test commit (wrong account)

--- encode.py
+++ encode.py
@@ -1,1 +1,3 @@
-# This will the base file for our encoding method.
+# This will the base file for our encoding method
+
+# algorithms to know: RSA should be good enough if we decide to make it a web app or something.
